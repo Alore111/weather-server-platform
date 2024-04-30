9900d4357d2c4a3ffbd4f9e5d8d9b5971221ac4d
Allllore <2050791391@qq.com>
2024-04-30 18:54:31 +0800

    初始化前端

---
+++ frontend/wsp-vue/vite.config.js
@@ -0,0 +1,25 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import VueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    VueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  // 配置前端服务地址和端口
+  server: {
+    host: '0.0.0.0',
+    port: 8092,
+    // 是否开启 https
+    https: false,
+  },
+})
